a960e40ad182fb828f0cd062ce62c12e469de381
Sriram K <fan4aram@gmail.com>
2024-04-21 05:13:52 +0530

    - Reads CSV file succesfully - Reads filename from flag succesfully

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fileName := readArguments()
+	problems := readCSV(fileName)
+
+	for {
+		problem, err := problems.Read()
+		if err == io.EOF {
+			break
+		}
+		checkErr(err)
+		fmt.Println(problem)
+	}
+}
+
+func readArguments() string {
+	fileName := flag.String(
+		"filename",
+		"problems.csv",
+		"Name of the CSV file with the problems and the solutions",
+	)
+
+	flag.Parse()
+
+	return *fileName
+}
+
+func readCSV(f string) *csv.Reader {
+	file, err := os.Open(f)
+	checkErr(err)
+	return csv.NewReader(file)
+}
+
+func checkErr(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
